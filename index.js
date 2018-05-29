7c402e4e2f414b683c157ca79f2f4d01bcd9f102
Yohannes Abera <yohannesns@gmail.com>
2018-05-29 02:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function theBeatlesPlay(musicians,instruments){
  const array = [];
   for(var i =0;i< musicians.length;i++){
-   array.push(musicians[i] + instruments[i])
+   array.push(musicians[i] + " " + instruments[i])
  }
  return array
+}
+function johnLennonFacts(fact){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+ while(facts[i]!= " "){
+  return facts[i] + !!!
+ }
 }
