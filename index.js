e8f3c26f06251823130b4bc9c288a3fd1ff841ff
Yohannes Abera <yohannesns@gmail.com>
2018-05-29 04:37:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function johnLennonFacts(fact){
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  "He hated the sound of his own voice"]
  var i = 0
  while(i < = facts.length){
   console.log( facts[i] + " !!!"
