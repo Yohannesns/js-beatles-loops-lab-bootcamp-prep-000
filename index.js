ecc48ea4af559d6a627d1009ddf393280436444a
Yohannes Abera <yohannesns@gmail.com>
2018-05-29 03:49:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function theBeatlesPlay(musicians,instruments){
  }
  return array
 }
-function johnLennonFacts(fact){
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
- var i = 0;
- while(i <= facts.length){
-  console.log( facts[i] + " !!!"
-   i++
- }
-}
+// function johnLennonFacts(fact){
+//   const facts = [
+//   "He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"
+// ];
+//  var i = 0;
+//  while(i < = facts.length){
+//   console.log( facts[i] + " !!!"
+//    i++
+//  }
+// }
 function iLoveTheBeatles(number){
   var array = [];
   var i = 0
@@ -25,5 +25,5 @@ function iLoveTheBeatles(number){
     array.push("I Love the Beatles")
     console.log(array[i] + " !");
 } while(i <15)
-      
+
   }
